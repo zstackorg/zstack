56b80893497acd89c701063054b34b02f5eb3714
Frank.Zhang <xin.zhang@mevoco.com>
2016-12-20 12:18:15 -0500

    working on rest api

---
+++ rest/src/main/groovy/RestDocumentationGenerator.groovy
@@ -0,0 +1,8 @@
+/**
+ * Created by xing5 on 2016/12/21.
+ */
+class RestDocumentationGenerator {
+
+    RestDocumentationGenerator() {
+    }
+}
---
+++ test/src/test/groovy/TestG.groovy
@@ -0,0 +1,11 @@
+import org.junit.Test
+
+/**
+ * Created by xing5 on 2016/12/21.
+ */
+class TestG {
+    @Test
+    void test() {
+        println("hello world")
+    }
+}

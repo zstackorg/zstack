9aee7f37c7ebbeeeef7cde7916e118d29dde322e
Ruan Shixin <shixin.ruan@zstack.io>
2018-03-23 20:05:56 +0800

    Add lb htttps feature
    Fix ZSTAC-10217

---
+++ sdk/src/main/java/org/zstack/sdk/CertificateInventory.java
@@ -0,0 +1,39 @@
+package org.zstack.sdk;
+
+
+
+public class CertificateInventory  {
+
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
+    public java.lang.String uuid;
+    public void setUuid(java.lang.String uuid) {
+        this.uuid = uuid;
+    }
+    public java.lang.String getUuid() {
+        return this.uuid;
+    }
+
+    public java.lang.String certificate;
+    public void setCertificate(java.lang.String certificate) {
+        this.certificate = certificate;
+    }
+    public java.lang.String getCertificate() {
+        return this.certificate;
+    }
+
+    public java.util.List listeners;
+    public void setListeners(java.util.List listeners) {
+        this.listeners = listeners;
+    }
+    public java.util.List getListeners() {
+        return this.listeners;
+    }
+
+}

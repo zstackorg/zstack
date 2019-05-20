75783fe751ca765c4fd7aac152b5499dc2c382b1
GuoYi <yi.guo@zstack.io>
2019-05-20 16:06:39 +0800

    create index for pci/mdev device vo

--- conf/db/upgrade/V3.5.0__schema.sql
+++ conf/db/upgrade/V3.5.0__schema.sql
@@ -7,6 +7,10 @@ ALTER TABLE `zstack`.`PciDeviceVO` ADD COLUMN `parentUuid` VARCHAR(32) DEFAULT N
 ALTER TABLE `zstack`.`PciDeviceVO` ADD COLUMN `pciSpecUuid` VARCHAR(32) DEFAULT NULL;
 ALTER TABLE `zstack`.`PciDeviceVO` ADD CONSTRAINT `fkPciDeviceVOPciDeviceVO` FOREIGN KEY (`parentUuid`) REFERENCES `PciDeviceVO` (`uuid`) ON DELETE CASCADE;
 ALTER TABLE `zstack`.`PciDeviceVO` ADD CONSTRAINT `fkPciDeviceVOPciDeviceSpecVO` FOREIGN KEY (`pciSpecUuid`) REFERENCES `PciDeviceSpecVO` (`uuid`) ON DELETE SET NULL;
+CREATE INDEX `idxPciDeviceVOtype` ON PciDeviceVO (`type`);
+CREATE INDEX `idxPciDeviceVOhostUuid` ON PciDeviceVO (`hostUuid`);
+CREATE INDEX `idxPciDeviceVOparentUuid` ON PciDeviceVO (`parentUuid`);
+CREATE INDEX `idxPciDeviceVOpciSpecUuid` ON PciDeviceVO (`pciSpecUuid`);
 
 ALTER TABLE `zstack`.`PciDeviceSpecVO` ADD COLUMN `type` VARCHAR(32) NOT NULL;
 ALTER TABLE `zstack`.`PciDeviceSpecVO` ADD COLUMN `state` VARCHAR(32) NOT NULL;
@@ -16,7 +20,7 @@ ALTER TABLE `zstack`.`PciDeviceSpecVO` ADD COLUMN `ramSize` VARCHAR(32) DEFAULT 
 
 ALTER TABLE `zstack`.`PciDeviceOfferingVO` ADD COLUMN `ramSize` VARCHAR(32) DEFAULT NULL;
 
-CREATE TABLE `zstack`.`MdevDeviceSpecVO` (
+CREATE TABLE IF NOT EXISTS `zstack`.`MdevDeviceSpecVO` (
     `uuid` VARCHAR(32) NOT NULL UNIQUE,
     `name` VARCHAR(32) NOT NULL,
     `description` VARCHAR(2048) DEFAULT NULL,
@@ -28,7 +32,7 @@ CREATE TABLE `zstack`.`MdevDeviceSpecVO` (
     PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `zstack`.`PciDeviceMdevSpecRefVO` (
+CREATE TABLE IF NOT EXISTS `zstack`.`PciDeviceMdevSpecRefVO` (
     `id` bigint unsigned NOT NULL UNIQUE AUTO_INCREMENT,
     `pciDeviceUuid` VARCHAR(32) NOT NULL,
     `mdevSpecUuid` VARCHAR(32) NOT NULL,
@@ -40,7 +44,7 @@ CREATE TABLE `zstack`.`PciDeviceMdevSpecRefVO` (
     CONSTRAINT `fkSpecRefMdevSpecUuid` FOREIGN KEY (`mdevSpecUuid`) REFERENCES `MdevDeviceSpecVO` (`uuid`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE `zstack`.`MdevDeviceVO` (
+CREATE TABLE IF NOT EXISTS `zstack`.`MdevDeviceVO` (
     `uuid` VARCHAR(32) NOT NULL UNIQUE,
     `name` VARCHAR(32) NOT NULL,
     `description` VARCHAR(2048) DEFAULT NULL,
@@ -54,6 +58,10 @@ CREATE TABLE `zstack`.`MdevDeviceVO` (
     `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
     `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
     PRIMARY KEY (`uuid`),
+    INDEX `idxMdevDeviceVOtype` (`type`),
+    INDEX `idxMdevDeviceVOhostUuid` (`hostUuid`),
+    INDEX `idxMdevDeviceVOparentUuid` (`parentUuid`),
+    INDEX `idxMdevDeviceVOmdevSpecUuid` (`mdevSpecUuid`),
     CONSTRAINT `fkMdevDeviceVOHostEO` FOREIGN KEY (`hostUuid`) REFERENCES `HostEO` (`uuid`) ON DELETE CASCADE,
     CONSTRAINT `fkMdevDeviceVOPciDeviceVO` FOREIGN KEY (`parentUuid`) REFERENCES `PciDeviceVO` (`uuid`) ON DELETE CASCADE,
     CONSTRAINT `fkMdevDeviceVOVmInstanceEO` FOREIGN KEY (`vmInstanceUuid`) REFERENCES `VmInstanceEO` (`uuid`) ON DELETE SET NULL,

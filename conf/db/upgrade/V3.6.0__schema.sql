094a47bb50152e5d3c32cb258f8080f41ee533c5
yaoning.li <2016220101025@std.uestc.edu.cn>
2019-07-08 09:12:27 +0800

    add IAM2OrganizationProjectRefVO

--- conf/db/upgrade/V3.6.0__schema.sql
+++ conf/db/upgrade/V3.6.0__schema.sql
@@ -192,3 +192,13 @@ DROP PROCEDURE IF EXISTS upgradeIAM2ReadRole;
 
 # delete dirty project admin attributes in db
 delete from IAM2VirtualIDAttributeVO where name = '__ProjectAdmin__' and value not in (select uuid from IAM2ProjectVO);
+
+CREATE TABLE `IAM2OrganizationProjectRefVO` (
+`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+`projectUuid` varchar(32) NOT NULL,
+`organizationUuid` varchar(32) NOT NULL,
+`lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+`createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+PRIMARY KEY (`id`),
+UNIQUE KEY `projectUuid` (`projectUuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=3624 DEFAULT CHARSET=utf8;

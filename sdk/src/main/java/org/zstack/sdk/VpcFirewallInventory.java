56ef38b023a6aa7042d06bb51ee96bfe837d30e9
yaohua.wu <yaohua.wu@zstack.io>
2019-07-16 11:06:27 +0800

    add update firewall api

--- sdk/src/main/java/org/zstack/sdk/VpcFirewallInventory.java
+++ sdk/src/main/java/org/zstack/sdk/VpcFirewallInventory.java
@@ -20,6 +20,14 @@ public class VpcFirewallInventory  {
         return this.vpcUuid;
     }
 
+    public java.lang.String name;
+    public void setName(java.lang.String name) {
+        this.name = name;
+    }
+    public java.lang.String getName() {
+        return this.name;
+    }
+
     public java.util.List interfaces;
     public void setInterfaces(java.util.List interfaces) {
         this.interfaces = interfaces;

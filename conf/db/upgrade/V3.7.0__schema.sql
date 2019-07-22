c6397733632b84f7c829e627ca59f49419228d94
Qi Le <qi.le@zstack.io>
2019-07-22 19:52:12 +0800

    SQL schema for SNSSmsEndpointVO, SNSSmsReceiverVO and AliyunSmsSNSTextTemplateVO. update sdk. build.pom dependency. add delete method to EnvSpec.

---
+++ conf/db/upgrade/V3.7.0__schema.sql
@@ -0,0 +1,42 @@
+CREATE TABLE `zstack`.`SNSSmsEndpointVO`
+(
+    `uuid` varchar(32) NOT NULL UNIQUE,
+    PRIMARY KEY (`uuid`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE `zstack`.`SNSSmsReceiverVO`
+(
+    `uuid`         varchar(32) NOT NULL UNIQUE,
+    `phoneNumber`  varchar(24) NOT NULL,
+    `endpointUuid` varchar(32) NOT NULL,
+    `type`         varchar(24) NOT NULL,
+    PRIMARY KEY (`uuid`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+CREATE TABLE `zstack`.`AliyunSmsSNSTextTemplateVO`
+(
+    `uuid`              varchar(32) NOT NULL UNIQUE,
+    `sign`              varchar(12) NOT NULL,
+    `alarmTemplateCode` varchar(13) NOT NULL,
+    `eventTemplateCode` varchar(13) NOT NULL,
+    `eventTemplate`     text,
+    PRIMARY KEY (`uuid`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8;
+
+# Foreign keys for table SNSSmsEndpointVO
+
+ALTER TABLE SNSSmsEndpointVO
+    ADD CONSTRAINT fkSNSSmsEndpointVOSNSApplicationEndpointVO FOREIGN KEY (uuid) REFERENCES SNSApplicationEndpointVO (uuid) ON UPDATE RESTRICT ON DELETE CASCADE;
+
+# Foreign keys for table SNSSmsReceiverVO
+
+ALTER TABLE SNSSmsReceiverVO
+    ADD CONSTRAINT fkSNSSmsReceiverVOSNSSmsEndpointVO FOREIGN KEY (endpointUuid) REFERENCES SNSSmsEndpointVO (uuid);
+
+# Foreign keys for table AliyunSmsSNSTextTemplateVO
+
+ALTER TABLE AliyunSmsSNSTextTemplateVO
+    ADD CONSTRAINT fkAliyunSmsSNSTextTemplateVOSNSTextTemplateVO FOREIGN KEY (uuid) REFERENCES SNSTextTemplateVO (uuid) ON UPDATE RESTRICT ON DELETE CASCADE;

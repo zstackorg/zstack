fc865b0d55af5f8494231907d72bea08e4c043ee
Qi Le <qi.le@zstack.io>
2019-08-05 19:41:15 +0800

    sdk updated

--- sdk/src/main/java/org/zstack/sdk/zwatch/alarm/sns/template/aliyunsms/UpdateAliyunSmsSNSTextTemplateAction.java
+++ sdk/src/main/java/org/zstack/sdk/zwatch/alarm/sns/template/aliyunsms/UpdateAliyunSmsSNSTextTemplateAction.java
@@ -70,6 +70,9 @@ public class UpdateAliyunSmsSNSTextTemplateAction extends AbstractAction {
     @Param(required = false)
     public String accessKeySecret;
 
+    @Param(required = false)
+    public String requestIp;
+
     @NonAPIParam
     public long timeout = -1;
 

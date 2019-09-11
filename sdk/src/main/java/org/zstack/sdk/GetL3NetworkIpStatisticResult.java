2aa69116f56e94caa5c65f3aca6dce9da39b294b
Qi Le <qi.le@zstack.io>
2019-09-11 14:03:35 +0800

    update sdk

---
+++ sdk/src/main/java/org/zstack/sdk/GetL3NetworkIpStatisticResult.java
@@ -0,0 +1,22 @@
+package org.zstack.sdk;
+
+
+
+public class GetL3NetworkIpStatisticResult {
+    public java.util.List ipStatistics;
+    public void setIpStatistics(java.util.List ipStatistics) {
+        this.ipStatistics = ipStatistics;
+    }
+    public java.util.List getIpStatistics() {
+        return this.ipStatistics;
+    }
+
+    public java.lang.Integer count;
+    public void setCount(java.lang.Integer count) {
+        this.count = count;
+    }
+    public java.lang.Integer getCount() {
+        return this.count;
+    }
+
+}

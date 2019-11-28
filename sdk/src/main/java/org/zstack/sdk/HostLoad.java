a4e25184f3980141185760ad1cf7bde46fe5d1e5
lining <2016220101025@std.uestc.edu.cn>
2019-11-28 21:08:19 +0800

    init drs

---
+++ sdk/src/main/java/org/zstack/sdk/HostLoad.java
@@ -0,0 +1,31 @@
+package org.zstack.sdk;
+
+
+
+public class HostLoad  {
+
+    public java.lang.String hostUuid;
+    public void setHostUuid(java.lang.String hostUuid) {
+        this.hostUuid = hostUuid;
+    }
+    public java.lang.String getHostUuid() {
+        return this.hostUuid;
+    }
+
+    public java.lang.Integer usedCPUPercent;
+    public void setUsedCPUPercent(java.lang.Integer usedCPUPercent) {
+        this.usedCPUPercent = usedCPUPercent;
+    }
+    public java.lang.Integer getUsedCPUPercent() {
+        return this.usedCPUPercent;
+    }
+
+    public java.lang.Integer usedMemoryPercent;
+    public void setUsedMemoryPercent(java.lang.Integer usedMemoryPercent) {
+        this.usedMemoryPercent = usedMemoryPercent;
+    }
+    public java.lang.Integer getUsedMemoryPercent() {
+        return this.usedMemoryPercent;
+    }
+
+}
